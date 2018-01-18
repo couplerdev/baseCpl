c95e0c7c4c08ec3913136f87f79b4da1a55034bf
NeoGeon <987378754@qq.com>
2018-01-18 15:53:36 +0800

    doc update

--- procManage/manage.cpp
+++ procManage/manage.cpp
@@ -9,13 +9,26 @@ typedef struct commInfo{
 	MPI_Comm comm;	
 	string desc;
 }commInfo;
+typedef struct model{
+	bool iamin;
+	bool iamroot;
+}model;
+typedef struct PFlag{
+	string name;
+	string type;
+	string value;
+}PFlag;
 class proc{
 private:
 	vector<commInfo> proc_comm;
+	vector<model> proc_model;
+	vector<PFlag> proc_flag;
 public:
 	proc();
 	proc(MPI_Comm comm);
+	void flag_setup(string fileName);
 	bool add_in(MPI_Comm comm);
+	void model_setup(string fileName);
 	void indentify();
 };
 
@@ -25,6 +38,9 @@ proc::proc(MPI_Comm comm){
 	MPI_Comm_size(comm,&commI.size);
 	commI.desc="WORLD";
 	proc_comm.push_back(commI);
+}
+void proc::flag_setup(string filename){
+	
 }
 bool proc::add_in(MPI_Comm comm){
 	
--- readme.md
+++ readme.md
@@ -2,7 +2,9 @@
 ## background
 This is a code aimed to implement the base requirement of a coupler for earth climate system.</br>
 Then we will update the code for a more comprehensive and complete version.
+The first version 0.0 may includes function  as follows: tansfer data, process management in a more coder friendly way , interpolation, time contol, schedule.
 
-## directory 
+## directory
+
 
 ## how to use
