71a83fcc94a133f0c2954aae9c0d1a45402aa49c
NeoGeon <987378754@qq.com>
2018-01-19 17:16:50 +0800

    add two subroutine to prom

--- procManage/main.F90
+++ procManage/main.F90
@@ -4,11 +4,11 @@ use procm
     implicit none
    
     type(proc) :: my_proc
-    !integer :: ierr
+    integer :: ierr
 
 
     call init(my_proc)
-    call printProc(my_proc)    
+    call printProc(my_proc, ierr)    
     call clean(my_proc)
 
 end program main
--- procManage/manage.F90
+++ procManage/manage.F90
@@ -30,6 +30,8 @@ include"mpif.h"
     end type proc
 
     public :: init
+    public :: add_model
+    public :: add_flag
     public :: printProc
     public :: clean
 
@@ -92,21 +94,79 @@ subroutine clean(my_proc)
 
 end subroutine clean
 
-subroutine printProc(my_proc)
+subroutine printProc(my_proc, ierr)
     implicit none
     type(proc), intent(inout) :: my_proc
-    integer :: iter    
+    integer :: iter
+    integer :: world_rank    
+    integer, intent(inout) :: ierr
 
+    ierr = 0
+    call MPI_Comm_rank(MPI_COMM_WORLD, world_rank, ierr)
     write(*,*) "my_proc:"
     if(my_proc%num_comms>=1) then
-         do iter = 1, my_proc%num_comms
-             write(*,*) "the comm: ",my_proc%comms(iter)%commName, "comm size:",&
-                        my_proc%comms(iter)%num_size, "my rank in comm:", &
-                        my_proc%comms(iter)%num_proc
-         end do
+        do iter = 1, my_proc%num_comms
+            write(*,*) "the comm: ",my_proc%comms(iter)%commName, "comm size:",&
+                       my_proc%comms(iter)%num_size, "my rank in comm:", &
+                       my_proc%comms(iter)%num_proc
+        end do
     end if
+    call MPI_Barrier(MPI_COMM_WORLD,ierr)
+    
+    if(world_rank==0) then
+        if(my_proc%num_flags>=1) then
+            do iter = 1, my_proc%num_flags
+                write(*,*) "the flags name:", my_proc%flags(iter)%flag_name, &
+                           "relative val:", my_proc%flags(iter)%val  
+            end do 
+        end if
+    end if
+
+    call MPI_Barrier(MPI_COMM_WORLD,ierr)
+    if(world_rank==0) then
+        if(my_proc%num_models>=1) then
+            do iter = 1, my_proc%num_models
+                write(*,*) "the model name:", my_proc%models(iter)%model_name
+            end do
+        end if
+     end if
 
 end subroutine printProc
 
+subroutine add_flag(my_proc, flag_name, val, ierr)
+    ! a bug!!! need to be grp to manage this
+    implicit none
+    type(proc), intent(inout) :: my_proc
+    character(len=20), intent(in) :: flag_name
+    integer, intent(in) :: val
+    integer, intent(inout) :: ierr
+
+    ierr = 0
+    my_proc%num_flags = my_proc%num_flags+1
+    if(my_proc%num_flags>MAX_SIZE)then
+        ierr = 1
+    else
+        my_proc%flags(my_proc%num_flags)%flag_name = flag_name
+        my_proc%flags(my_proc%num_flags)%val = val
+    end if
+
+end subroutine add_flag
+
+subroutine add_model(my_proc, model_name, ierr)
+
+    implicit none
+    type(proc), intent(inout) :: my_proc
+    character(len=20), intent(in) :: model_name
+    integer, intent(inout) :: ierr
+
+    ierr = 0
+    my_proc%num_models = my_proc%num_models+1
+    if(my_proc%num_models>MAX_SIZE)then
+        ierr = 1
+    else
+        my_proc%models(my_proc%num_models)%model_name = model_name
+    end if
+
+end subroutine add_model
 
 end module procM
