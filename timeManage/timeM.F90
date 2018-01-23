3f53aaa47ce1f039a61684c2f4f9fd44846d148f
NeoGeon <987378754@qq.com>
2018-01-23 09:44:13 +0800

    add a subroutine in timeM

---
+++ timeManage/timeM.F90
@@ -0,0 +1,22 @@
+module timeM
+    
+    implicit none
+    type EClock
+        integer :: clock
+    end type EClock
+
+    public :: clock_advance
+    public :: triger
+contains 
+
+subroutine clock_advance(EClock)
+
+end subroutine clock_advance
+
+subroutine triger(EClock, flag)
+
+
+
+end subroutine triger
+
+end module timeM
