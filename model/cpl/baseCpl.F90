21dc77947fee56b2c84948f35d2ae83b81e71d51
NeoGeon <987378754@qq.com>
2018-01-23 09:44:39 +0800

    baseCpl.F90 update

--- model/cpl/baseCpl.F90
+++ model/cpl/baseCpl.F90
@@ -1,14 +1,22 @@
 module baseCpl
-
+use procm ,only: pm_init => init 
+use cpl_vect
     implicit none
+
+    type(proc) :: my_proc
+
     public :: cpl_init
     public :: cpl_run
     public :: cpl_final
-
+     
 contains 
 
 subroutine cpl_init()
-
+    
+    implicit none
+    call init(my_proc)
+    
+    
 
 end subroutine cpl_init
 
